afbf53cb203313fdd5694864bb2fff5c084eba4e
Yassir <yassirsamhadana@icloud.com>
2020-07-20 09:24:19 +0300

    End of Section 3

---
+++ DatingApp.API/Data/IAuthRepository.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User uSer, string password);
+         Task<User> login(string username, string password);
+         Task<bool> UserExists(string username);
+
+    }
+}
