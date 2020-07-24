2a209ef47143f923017b1c7b38f5ea2f20d71eec
Yassir <yassirsamhadana@icloud.com>
2020-07-24 14:44:41 +0300

    End of Section 8

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -9,8 +9,6 @@ namespace DatingApp.API.Models.Data
         public DataContext(DbContextOptions<DataContext> options) : base (options) {}
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
-        
-            
-        
+        public DbSet<Photo> Photos { get; set; }
     }
 }
